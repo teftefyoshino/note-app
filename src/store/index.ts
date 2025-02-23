bcc2a886162f5325d7f4a676ccb00ae157c62e09
teftefyoshino <takanori.yoshino@willof.co.jp>
2025-02-24 01:44:05 +0900

    Add editor function

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,3 +11,17 @@ export const selectedNoteAtom = atom((get) => {
 
     return notes.find((note) => note.id === id) || null;
 });
+
+export const saveNoteAtom = atom(null, (get, set, newContent: string) => {
+    const note = get(selectedNoteAtom);
+    if (!note) return;
+
+    const updatedNote = new Note(note.id, note.title, newContent, Date.now());
+    const notes = get(notesAtom);
+    const updatedNotes = notes.map((n) => {
+      if (n.id !== note.id) return n;
+      return updatedNote;
+    });
+
+    set(notesAtom, updatedNotes);
+});
